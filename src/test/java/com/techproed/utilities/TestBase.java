ed71ceea84a62a218318076a606637b38b97d9f7
Salih1-55 <buenosaires.sy19@gmail.com>
2020-09-04 17:16:43 -0300

    DERS 13 SONU

---
+++ src/test/java/com/techproed/utilities/TestBase.java
@@ -0,0 +1,31 @@
+package com.techproed.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+   //private sadece ayni clasta
+   //default ayni pakette
+   //protected ayni paket ve claslarda
+   //public hepsinde
+public abstract class TestBase {
+
+  protected WebDriver driver;
+
+  @BeforeClass
+    public void setUp(){
+      WebDriverManager.chromedriver().setup();
+      driver = new ChromeDriver();
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+  }
+ @AfterClass
+    public void tearDown(){
+     // driver.quit();
+ }
+
+}
