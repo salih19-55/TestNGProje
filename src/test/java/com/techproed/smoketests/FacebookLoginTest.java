8ca1d4814379d7f6597554bc149ad0c05879517d
Salih1-55 <buenosaires.sy19@gmail.com>
2020-09-11 17:20:19 -0300

    day 19 ders sonu

---
+++ src/test/java/com/techproed/smoketests/FacebookLoginTest.java
@@ -0,0 +1,24 @@
+package com.techproed.smoketests;
+
+import com.techproed.pages.FacebookLoginPage;
+import com.techproed.utilities.ConfigurationReader;
+import com.techproed.utilities.TestBase;
+import org.testng.annotations.Test;
+
+public class FacebookLoginTest extends TestBase {
+
+    @Test
+    public void girisTesti(){
+        driver.get("http://facebook.com");
+
+        FacebookLoginPage loginPage = new FacebookLoginPage(driver);
+        loginPage.emailKutusu.sendKeys(ConfigurationReader.getProperty("fb_email"));
+        loginPage.sifreKutusu.sendKeys(ConfigurationReader.getProperty("fb_sifre"));
+
+
+
+
+    }
+
+
+}
